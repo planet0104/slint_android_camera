ff93cf5e747b7ae016497c94bc928959488aa326
Jia Ye <planet0104@gmail.com>
2024-03-18 18:57:50 +0800

    hello world

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#[no_mangle]
+fn android_main(app: slint::android::AndroidApp) {
+    slint::android::init(app).unwrap();
+    slint::slint!{
+        export component MainWindow inherits Window {
+            Text { text: "Hello World"; }
+        }
+    }
+    MainWindow::new().unwrap().run().unwrap();
+}
